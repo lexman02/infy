f2827ae45f12a96b0bba6718ca7b8ca45eab1593
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-02-14 17:46:02 -0500

    Init Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    
+                </NavMenu>
+            </Nav>
+        </>
+    );
+}
+
+export default Navbar;
