c502e3deed2a0c7aeb620bd0a8f57a3216fff790
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-02-19 18:12:55 -0500

    feat(front): init signup form

---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,12 @@
+export default function Signup(){
+    return (
+        <div>
+            <h1>Fill out your INFYnaut Application!</h1>
+            <form>
+                <label for="username">Username:</label><br/>
+                <input type="text" id="username" name="username"></input><br/>
+                
+            </form>
+        </div>
+    )
+}
