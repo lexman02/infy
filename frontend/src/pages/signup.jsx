42038204928abfb177815b0c728c71ce992f231d
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-02-22 14:55:04 -0500

    Add Signup Conversion to JSON function

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -19,3 +19,11 @@ export default function Signup(){
         </div>
     )
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formData = new FormData(e.target)
+  const json = JSON.stringify(Object.fromEntries(formData));
+  alert("Signup test")
+})
