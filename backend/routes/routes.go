659ffd085bf5f1ef4d4cf9db65118f51fadf4577
xaviermcneill <mcneillx20@students.ecu.edu>
2024-02-24 02:48:03 -0500

    fix(back): add cors package to connect frontend

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func InitRoutes() *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "Cache-Control"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Initialize the routes
 	AuthRoutes(router)
