8a9b1a42a30da1b7e879afae0d884ba1ad85f349
JayWasTakenWasTaken <moorejord20@students.ecu.edu>
2024-03-23 01:19:44 +0000

    Very ugly and doesn't connect to API yet but it's a bedrock for now. Will update in the near future

--- frontend/src/pages/favorites.jsx
+++ frontend/src/pages/favorites.jsx
@@ -1,7 +1,39 @@
-export default function Favorites(){
+import React, { useState } from "react";
+
+export default function Favorites() {
+    const [movies, setMovies] = useState([]);
+    const [inputValue, setInputValue] = useState([]);
+
+    const addMovie = () => {
+        if (inputValue) {
+            setMovies([...movies, inputValue]);
+            setInputValue('');
+        }
+    };
+
+    const removeMovie = (index) => {
+        const newMovies = movies.filter((_, i) => i !== index);
+        setMovies(newMovies);
+    };
+
+
     return (
-        <>
-            <h1 className="text-neutral-50">Favorites</h1>
-        </>
+        <div className="searchBar">
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Search a movie!"
+            />
+            <button onClick={addMovie}>Add</button>
+            <ul>
+                {movies.map((movie, index) => (
+                    <li key={index}>
+                        {movie}
+                        <button onClick={() => removeMovie(index)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-} 
+}
