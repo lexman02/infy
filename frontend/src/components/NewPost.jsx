c0c737ce022fa989e065d47602d8d44bd6baab18
xaviermcneill <mcneillx20@students.ecu.edu>
2024-04-14 17:55:52 -0400

    feat: add movie to watched list on new post (ESA-85)

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -14,14 +14,25 @@ export default function NewPost({ onNewPost }) {
         e.preventDefault();
         const data = Object.fromEntries(new FormData(e.target));
         data.movie_id = selectedMovie.id.toString();
-        await axios.post('http://localhost:8000/posts/', data, {withCredentials: true})
+        await axios.post('http://localhost:8000/posts/', data, { withCredentials: true })
             .then(() => {
                 setSelectedMovie(null);
+
+                if (data.watched) {
+                    axios.post(`http://localhost:8000/profile/movies/add/watched`, { 'movieId': data.movie_id }, { withCredentials: true })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                }
+
                 onNewPost();
             })
             .catch(error => {
                 console.error(error);
             });
+
+
+        onNewPost();
     }
 
     return (
@@ -42,11 +53,18 @@ export default function NewPost({ onNewPost }) {
                     <form onSubmit={handleSubmit}>
                         <div className="mt-6 relative">
                             <label htmlFor="content" className="block mb-2 text-md font-medium"></label>
-                            <textarea id="content" name="content" placeholder="Anything to say?" className="border rounded-lg border-indigo-500/30 bg-indigo-950/70 text-neutral-200 shadow-inner block w-full p-2.5" data-gramm="false" autoComplete="off"/>
                             {/* disable auto complete and grammarly */}
-                            <button className="absolute bottom-2 right-2 bg-indigo-900 hover:bg-indigo-800 text-neutral-50 rounded-full p-2">
-                                <PaperAirplaneIcon className="h-4 w-4" />
-                            </button>
+                            <textarea id="content" name="content" placeholder="Anything to say?" className="border rounded-t-lg border-indigo-900 bg-indigo-950/70 text-neutral-200 shadow-inner block w-full p-2.5" data-gramm="false" autoComplete="off" />
+                            <div className="border border-t-0 border-indigo-900 rounded-b-lg flex items-center justify-between p-2">
+                                <label className="inline-flex items-center cursor-pointer">
+                                    <input type="checkbox" name="watched" value="true" className="sr-only peer" defaultChecked />
+                                    <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
+                                    <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Add movie to watched?</span>
+                                </label>
+                                <button className="bg-indigo-600 hover:bg-indigo-800 text-neutral-50 rounded-full p-2">
+                                    <PaperAirplaneIcon className="h-4 w-4" />
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
